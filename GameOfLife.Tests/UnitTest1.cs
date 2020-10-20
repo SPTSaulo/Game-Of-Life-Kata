5636895006fc03dc760dee810dd98661647173b0
Saulo Santana Batista (AIDA) <sausantana@gda.dis>
2020-10-20 13:55:01 +0100

    Monodimensional arrays done

--- GameOfLife.Tests/UnitTest1.cs
+++ GameOfLife.Tests/UnitTest1.cs
@@ -9,24 +9,46 @@ namespace Tests
     {
 
         [Test]
-        public void Generate_board_with_one_die_cell()
+        public void Generate_board_with_one_live_cell()
         {
-            char[] board = new char [] {'-'};
+            int[] board = new int [] {1};
 
-            char[] generateBoard = Game.EvaluateBoard(board);
+            int[] generateBoard = Game.GenerateNextGenerationBoard(board);
 
-            char[] expectedBoard = new char[] {'-'};
+            int[] expectedBoard = new int[] {0};
             generateBoard.Should().BeEquivalentTo(expectedBoard);
         }
 
         [Test]
         public void Generate_board_with_one_die_cell_and_one_live_cell()
         {
-            char[] board = new char[] {'-','*'};
+            int[] board = new int[] {0, 1};
 
-            char[] generateBoard = Game.EvaluateBoard(board);
+            int[] generateBoard = Game.GenerateNextGenerationBoard(board);
 
-            char[] expectedBoard = new char[] {'*','*'};
+            int[] expectedBoard = new int[] {0, 0};
+            generateBoard.Should().BeEquivalentTo(expectedBoard);
+        }
+
+        [Test]
+        public void Generate_board_with_three_live_cell()
+        {
+            int [] board = new int[] {1, 1, 1};
+
+            int[] generateBoard = Game.GenerateNextGenerationBoard(board);
+
+            int[] expectedBoard = new int[] {0, 1, 0};
+            generateBoard.Should().BeEquivalentTo(expectedBoard);
+        }
+
+        [Test]
+        public void Generate_board_with_one_live_cell_between_two_die_cell()
+        {
+            int [] board = new int[] {0, 1, 0};
+
+            int[] generateBoard = Game.GenerateNextGenerationBoard(board);
+
+            int[] expectedBoard = new int[] {0, 0, 0};
             generateBoard.Should().BeEquivalentTo(expectedBoard);
         }
     }
--- GameOfLife/Game.cs
+++ GameOfLife/Game.cs
@@ -1,19 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GameOfLife
 {
     public class Game
     {
-        public static char[] EvaluateBoard(char [] board)
+        public static int[] GenerateNextGenerationBoard(int[] board)
+        {
+            int [] neighborsAlivePerCell = new int[board.GetLength(0)];
+            updateNeighborsAlivePerCell(neighborsAlivePerCell, board);
+            updateBoard(neighborsAlivePerCell, board);
+            return board;
+        }
+
+        private static void updateBoard(int[] neighborsAlivePerCell, int[] board)
+        {
+            for (int i = 0; i < neighborsAlivePerCell.Length; i++)
+            {
+                board[i] = neighborsAlivePerCell[i] == 2 && board[i] == 1 ? 1 : 0;
+            }
+        }
+
+        private static void updateNeighborsAlivePerCell(int[] neighborsAlivePerCell, int[] board)
         {
             for (int i = 0; i < board.GetLength(0); i++)
             {
-                return null;
+                neighborsAlivePerCell[i] = CountLivingNeigbors(i, board);
             }
-
-            return null;
         }
 
+        private static int CountLivingNeigbors(int i, int[] board)
+        {
+            int count = 0;
+            count += CountMiddleLivingNeigbors(i, board);
+            count += CountStartLivingNeighbors(i, board);
+            count += CountEndLivingNeighbors(i, board);
+            return count;
+        }
+
+        private static int CountEndLivingNeighbors(int i, int[] board)
+        {
+            int count = 0;
+            if (i == board.GetLength(0) - 1 && board.GetLength(0) > 1)
+            {
+                if (board[i - 1] == 1) count++;
+            }
+            return count;
+        }
+
+        private static int CountStartLivingNeighbors(int i, int[] board)
+        {
+            int count = 0;
+            if (i == 0 && board.GetLength(0) > 1)
+            {
+                if (board[i + 1] == 1) count++;
+            }
+
+            return count;
+        }
+
+        private static int CountMiddleLivingNeigbors(int i, int[] board)
+        {
+            int count = 0;
+            if (i < board.GetLength(0) - 1 && i > 0)
+            {
+                if (board[i - 1] == 1) count++;
+                if (board[i + 1] == 1) count++;
+            }
+            return count;
+        }
     }
 }
 
